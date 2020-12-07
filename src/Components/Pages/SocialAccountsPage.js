34f659554d90c92b3f82ed11f91d092920f03dd6
Christopher Barnes <chris.barnes.2000@me.com>
2020-12-06 21:19:57 -0800

    [Update]: Social Accounts Page

--- src/Components/Pages/SocialAccountsPage.js
+++ src/Components/Pages/SocialAccountsPage.js
@@ -1,85 +1,33 @@
 import React, { Component } from "react";
-import { Link } from "react-router-dom";
-import { HOME } from "../Routes";
-
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
 
 class SocailAccountsPage extends Component {
   render() {
     return (
       <div className="container social-accounts-container">
-        <div>
-          <a
-            className="nav-item nav-link"
-            id="nav-linkedin-tab"
-            data-toggle="tab"
-            role="tab"
-            aria-controls="nav-linkedin"
-            aria-selected="false"
-            href="https://www.linkedin.com/in/chrisbarnes2000/"
-          >
+        <div className="page-inner">
+          <a href="https://www.linkedin.com/in/chrisbarnes2000/">
             <FontAwesomeIcon icon={["fab", "linkedin"]} color="#0e76a8" size="2x" />
             {/* <FontAwesomeIcon icon={["fas", "external-link-alt"]} /> */}
           </a>
-          <a
-            className="nav-item nav-link"
-            id="nav-github-tab"
-            data-toggle="tab"
-            role="tab"
-            aria-controls="nav-github"
-            aria-selected="false"
-            href="https://github.com/ChrisBarnes7404"
-          >
+          <a href="https://github.com/ChrisBarnes7404">
             <FontAwesomeIcon icon={["fab", "github"]} color="#211F1F" size="2x" />
             {/* <FontAwesomeIcon icon={["fas", "external-link-alt"]} /> */}
           </a>
-          <a
-            className="nav-item nav-link"
-            id="nav-instagram-tab"
-            data-toggle="tab"
-            role="tab"
-            aria-controls="nav-instagram"
-            aria-selected="false"
-            href="https://instagram.com/dragon_dominant"
-          >
+          <a href="https://instagram.com/dragon_dominant">
             <FontAwesomeIcon icon={["fab", "instagram"]} color="#bc2a8d" size="2x" />
             {/* <FontAwesomeIcon icon={["fas", "external-link-alt"]} /> */}
           </a>
-        </div>
-        <div>
-          <a
-            className="nav-item nav-link"
-            id="nav-twitter-tab"
-            data-toggle="tab"
-            role="tab"
-            aria-controls="nav-twitter"
-            aria-selected="false"
-            href="https://twitter.com/dragon_dominant"
-          >
+          <a href="https://twitter.com/dragon_dominant">
             <FontAwesomeIcon icon={["fab", "twitter"]} color="#00acee" size="2x" />
             {/* <FontAwesomeIcon icon={["fas", "external-link-alt"]} /> */}
           </a>
-          <a
-            className="nav-item nav-link"
-            id="nav-facebook-tab"
-            data-toggle="tab"
-            role="tab"
-            aria-controls="nav-facebook"
-            aria-selected="false"
-            href="https://facebook.com/chrisb7404"
-          >
+          <a href="https://facebook.com/chrisb7404">
             <FontAwesomeIcon icon={["fab", "facebook"]} color="#3b5998" size="2x" />
             {/* <FontAwesomeIcon icon={["fas", "external-link-alt"]} /> */}
           </a>
-          <a
-            className="nav-item nav-link"
-            id="nav-youtube-tab"
-            data-toggle="tab"
-            role="tab"
-            aria-controls="nav-youtube"
-            aria-selected="false"
-            href="https://youtube.com/channel/UCZ13zqecjflTKbwEfT9qiIg"
-          >
+          <a href="https://youtube.com/channel/UCZ13zqecjflTKbwEfT9qiIg">
             <FontAwesomeIcon icon={["fab", "youtube"]} color="#c4302b" size="2x" />
             {/* <FontAwesomeIcon icon={["fas", "external-link-alt"]} /> */}
           </a>
